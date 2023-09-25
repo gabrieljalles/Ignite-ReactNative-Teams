6f6df0e633471e9d3c63888f1d77865bd7e40d7d
Gabriel Jalles Ferreira Mota <gabriel.jalleswork@hotmail.com>
2023-09-25 09:44:31 -0300

    PlayerCard

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,14 +1,16 @@
 import { Highlight } from '@components/Highlight';
-import { Container, Form } from './styles';
+import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
 import { Header } from 'react-native/Libraries/NewAppScreen';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Input } from '@components/Input';
 import { FlatList } from 'react-native';
 import React, { useState } from 'react';
 import { Filter } from '@components/Filter';
+import { PlayerCard } from '@components/PlayerCard';
 
 export function Players() {
-    const [team, setTeam] = useState(' Time A')
+    const [team, setTeam] = useState(' Time A');
+    const [players, setPlayers] = useState<string[]>(['']);
     return (
         <Container>
             <Header showBackButton />
@@ -29,20 +31,36 @@ export function Players() {
                 />
             </Form>
 
+            <HeaderList>
+                <FlatList
+                    data={['Time A', 'Time B']}
+                    keyExtractor={item => item}
+                    renderItem={({ item }) => (
+                        <Filter
+                            title={item}
+                            isActive={item === team}
+                            onPress={() => setTeam(item)}
+                        />
+                    )}
+                    horizontal
+                />
+
+                <NumberOfPlayers>
+                        {players.length}
+                </NumberOfPlayers>
+
+            </HeaderList>
+
             <FlatList
-                data={['Time A', 'Time B']}
+                data={players}
                 keyExtractor={item => item}
-                renderItem={({ item }) => (
-                    <Filter
-                        title={item}
-                        isActive={item === team}
-                        onPress={() => setTeam(item)}
+                renderItem={({item}) => (
+                    <PlayerCard 
+                    name={item} 
+                    onRemove={() => {}} 
                     />
                 )}
-                horizontal
             />
-
-
         </Container>
     )
 }
